4a1f8af995978348a78282ba7b815b22a0865a02
zhangyawei <zyw568579931@163.com>
2018-09-03 16:32:32 +0800

    111

--- src/page/test/index.js
+++ src/page/test/index.js
@@ -1,17 +1,31 @@
 import React, {Component} from 'react';
 import { Button ,Input } from 'antd';
-
+import axios from 'axios'
 
 class Test extends Component {
     constructor(props) {
         super(props)
         this.state = {
             loading: true,
-            arr: [1,2,3,4],
+            arr: [],
             inputValue: 'input'
         }
     }
+    componentWillMount() {
 
+    }
+    componentDidMount() {
+        axios.get('http://192.168.1.20:9428/api/warn_info').then(res=> {
+            console.log(res.data)
+            this.setState({
+                arr: res.data
+            });
+        })
+        console.log(this.refs.demo)
+        this.refs.demo.onclick = () => {
+            console.log(this.refs.demo)
+        }
+    }
 
     handleClick() {
         alert(33)
@@ -21,12 +35,30 @@ class Test extends Component {
             inputValue: e.target.value
         })
     }
+
+    listcs() {
+        if (this.state.loading) {
+            return <div>loadng</div>
+        }
+    }
     render() {
+        let ifelse = null;
+        if(this.state.loading) {
+            ifelse = <div>条件判断</div>
+        }else{
+            ifelse = ''
+        }
         return (
                 <div>
                     <Button type="primary" onClick={this.handleClick.bind(this)} shape="circle" icon="search" />
                     <Input placeholder="Basic usage" value={this.state.inputValue} onChange={this.inputHandleChange.bind(this)} />
-                    {this.state.inputValue}
+                    <ul>
+                        <p ref="demo">列表渲染</p>
+                        {this.state.arr.map((item,index)=> {
+                            return <li key={index}>{item.event}</li>
+                        })}
+                    </ul>
+                    {ifelse}
                 </div>
         );
     }
