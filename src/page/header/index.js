d70279ba29f614feeaef4022124e281f57653d83
zhangyawei <zyw568579931@163.com>
2018-11-13 14:29:45 +0800

    增加导航菜单

--- src/page/header/index.js
+++ src/page/header/index.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
-import { Tag } from 'antd';
+import { Row, Col } from 'antd';
+import { NavLink } from 'react-router-dom';
+import './index.css'
+
+const selectedStyle = {
+    backgroundColor: 'white',
+    color: 'aqua'
+}
 
 class Header extends Component {
     render() {
         return (
-            <div>header组件 <Tag color="cyan">{this.props.name}</Tag></div>
+            <Row gutter={24}>
+                <Col span={12} className="menu-item">
+                    <NavLink to='/' exact activeStyle ={selectedStyle}>主页</NavLink>
+                </Col>
+                <Col span={12} className="menu-item">
+                    <NavLink to='/clock' activeStyle ={selectedStyle}>测试页</NavLink>
+                </Col>
+            </Row>
         );
     }
 }
--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,20 +1,17 @@
 import React, {Component} from 'react';
-import { Route, Link} from 'react-router-dom';
+import { Router, Route, Link} from 'react-router-dom';
 import './index.css'
 import Test from '../test'
 import Header from '../header'
 import Clock from '../../components/Clock'
 
 class home extends Component {
-    state = {
-        headerName: 'header属性'
-    }
     render() {
         return (
             <div className="home">
-                <Header name={this.state.headerName} />
-                <Clock />
-                <Route path="/" component={Test} />
+                <Header />
+                <Route path="/" exact component={Test} />
+                <Route path="/clock" component={Clock} />
                 <div><Link to="/login">登录</Link></div>
             </div>
         );
